5b9a341ad76b9f91622e22e1d9166810602d1281
shadrach_05 <shadrachmulia@gmail.com>
2025-04-11 21:35:20 +0700

    feat: custom sidebar commented

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Sidebar from '@/components/dashboard/Sidebar';
+// import Sidebar from '@/components/dashboard/Sidebar';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/dashboard/app-sidebar';
 
