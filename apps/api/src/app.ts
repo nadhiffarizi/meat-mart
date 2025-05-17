dd0da851510e61d71f5d53f22a2f87868d6a0c62
Nadhif Farizi <farizinadhif@gmail.com>
2025-05-17 23:40:34 +0700

    feat: uncomment order cron

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -110,7 +110,7 @@ export default class App {
 
   private cron(): void {
     deadlinePayment().start() // cron for deadline payment
-    // orderConfirmation().start() // cron for order confirmation
+    orderConfirmation().start() // cron for order confirmation
   }
 
   public start(): void {
