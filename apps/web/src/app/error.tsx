6fd5ce9759e7dbb38ee3fcaa31f5518c1f5b3e50
Nadhif Farizi <farizinadhif@gmail.com>
2025-05-18 13:47:10 +0700

    feat: add error page

---
+++ apps/web/src/app/error.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! We couldn’t find what you’re looking for.</p>
+    </div>
+  );
+}
